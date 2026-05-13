8339a10519d53a94424a034b52fb9c436c598530
Anshuman <anshumanpachauri26@gmail.com>
2026-05-13 12:00:00 +0530

    Setup the localServer port and baseURI for testing environment.

--- src/test/java/com/ap/microservices/order/OrderServiceApplicationTests.java
+++ src/test/java/com/ap/microservices/order/OrderServiceApplicationTests.java
@@ -1,11 +1,15 @@
 package com.ap.microservices.order;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
 import org.testcontainers.mysql.MySQLContainer;
+
+import io.restassured.RestAssured;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -13,6 +17,15 @@ class OrderServiceApplicationTests {
 
 	@ServiceConnection
 	static MySQLContainer mySQLContainer = new MySQLContainer("mysql:latest");
+	
+	@LocalServerPort
+	private Integer port;
+	
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
 	
 	@Test
 	void contextLoads() {
