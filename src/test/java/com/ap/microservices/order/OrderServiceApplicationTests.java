21edfdb5fdeb884d4aa79f13ce9efdaef350c42b
Anshuman <anshumanpachauri26@gmail.com>
2026-05-13 12:00:00 +0530

    Added restAssured dependency and created MySQLContainer for testing.

--- src/test/java/com/ap/microservices/order/OrderServiceApplicationTests.java
+++ src/test/java/com/ap/microservices/order/OrderServiceApplicationTests.java
@@ -2,12 +2,18 @@ package com.ap.microservices.order;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.mysql.MySQLContainer;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class OrderServiceApplicationTests {
 
+	@ServiceConnection
+	static MySQLContainer mySQLContainer = new MySQLContainer("mysql:latest");
+	
 	@Test
 	void contextLoads() {
 	}
