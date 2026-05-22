5f3b65597754448631825bab12b3fb3a7623d7d4
Anshuman <anshumanpachauri26@gmail.com>
2026-05-22 12:00:00 +0530

    Changed the usage of open feign to rest client i.e. latest http request sender via a microservice to another.

--- src/main/java/com/ap/microservices/order/client/InventoryClient.java
+++ src/main/java/com/ap/microservices/order/client/InventoryClient.java
@@ -1,6 +1,6 @@
 package com.ap.microservices.order.client;
 
-import org.springframework.cloud.openfeign.FeignClient;
+//import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
